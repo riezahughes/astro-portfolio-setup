ea571fc964a455918339c4c34a1259ffa19a4e4d
Dan <dan@tantalus-troupe.com>
2022-08-21 17:37:42 +0100

    test udpates

--- src/components/ComputerCodeLine/ComputerCodeLine.test.ts
+++
@@ -1,7 +0,0 @@
-import ComputerCodeLine from "./ComputerCodeLine";
-
-describe("line example", () => {
-	it("should just run", () => {
-		expect(true).toBe(true);
-	});
-});
---
+++ src/components/ComputerCodeLine/ComputerCodeLine.test.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { render } from "@testing-library/react";
+import ComputerCodeLine from "./ComputerCodeLine";
+
+describe("Testing custom component and husky", () => {
+	it("can at the very least render the component", async () => {
+		const { getByRole } = render(<ComputerCodeLine user="dan@test" path="~123/" words="npm install" />);
+		expect(getByRole("heading", { level: 1 })).toBeInTheDocument();
+		expect(getByRole("heading", { level: 1 })).toHaveTextContent("dan@test");
+		expect(getByRole("heading", { level: 1 })).toHaveTextContent("~123/");
+		expect(getByRole("heading", { level: 1 })).toHaveTextContent("npm install");
+	});
+});
