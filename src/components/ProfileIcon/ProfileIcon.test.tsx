c182d8967495d15ad3b0685577a983a67f31f871
Dan <dan@tantalus-troupe.com>
2022-09-10 13:41:53 +0100

    minor test changes

--- src/components/ProfileIcon/ProfileIcon.test.tsx
+++ src/components/ProfileIcon/ProfileIcon.test.tsx
@@ -1,12 +1,11 @@
 import React from "react";
 import { render } from "@testing-library/react";
 import ProfileIcon from "./ProfileIcon";
-import { AiFillLinkedin, AiFillFacebook, AiFillHeart } from "react-icons/ai";
+import { AiFillFacebook, AiFillHeart } from "react-icons/ai";
 
 describe("Using module: ProfileIcon", () => {
 	it("Can see the icon appear", () => {
-		const { getByText, getByRole } = render(<ProfileIcon url="#123" icon={<AiFillFacebook title="facebook" />} />);
+		const { getByText, getByRole } = render(<ProfileIcon url="#123" icon={AiFillFacebook} />);
 		expect(getByRole("link")).toHaveAttribute("href", "#123");
-		expect(getByText("facebook")).toBeInTheDocument();
 	});
 });
--- src/components/ProfileIcon/ProfileIcon.tsx
+++ src/components/ProfileIcon/ProfileIcon.tsx
@@ -2,7 +2,7 @@ import type { IProfileIcon } from "./types";
 import styles from "./styles.module.css";
 
 const ProfileIcon = ({ icon, url }: IProfileIcon) => {
-	return <a href={url}>{icon}</a>;
+	return <a href={url}>{icon()}</a>;
 };
 
 export default ProfileIcon;
